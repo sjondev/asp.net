fd3fcb5aac93651839c7929c4e20237a677f0b31
sjondev <jonsilvasantos7@gmail.com>
2025-02-01 16:14:08 -0300

    adding Readme.md

---
+++ Readme.md
@@ -0,0 +1,2 @@
+# Fundamentos do ASP.NET
+* Estou utilizando a versão 8 do .NET para fazer meus estudos e trabalhar bem nesses fundamentos
--- obj/Debug/net8.0/TodoList.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4e29d0a1240d5e322721e01b3da46eedc75b966")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
